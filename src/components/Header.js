b97be831b33da32d400367bf4b954b8bdaf7ccd4
Anthony Agnone <anthonyagnone@Anthonys-MacBook-Pro.local>
2019-03-27 15:09:14 -0400

    HeaderCrossSection

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,21 @@
-import React from 'react'
-import Nav from './Nav'
+import React from "react";
+import Nav from "./Nav";
 
 const Header = () => {
   return (
     <header>
       {/* <Nav /> */}
-      div.vertical  
+      <div className="crossSection">
+        <div className="verticalTop" />
+        <div className="verticalBottom" />
+        <div className="horizontalLeft" />
+        <div className="horizontalRight" />
+      </div>
+      <div className="logo">
+        <h1>AD</h1>
+      </div>
     </header>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
