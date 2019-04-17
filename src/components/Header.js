7d0edcda95fdd5a8e3582be5f36b0609fdfebf8d
Anthony Agnone <anthonyagnone@Anthonys-MacBook-Pro.local>
2019-04-17 11:25:18 -0400

    Added more things

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,21 +1,51 @@
-import React from "react";
-import Nav from "./Nav";
+import React, { Component } from "react";
+import { Wrapper, Button, Menu, MenuItem } from "react-aria-menubutton";
+// import Nav from "./Nav";
 
-const Header = () => {
-  return (
-    <header>
-      {/* <Nav /> */}
-      <div className="crossSection">
-        <div className="verticalTop" />
-        <div className="verticalBottom" />
-        <div className="horizontalLeft" />
-        <div className="horizontalRight" />
-      </div>
-      <div className="logo">
-        <h1>AD</h1>
-      </div>
-    </header>
-  );
-};
+const menuItemWords = ["foo", "bar", "baz"];
+export default class Header extends Component {
+  constructor(props) {
+    super(props);
 
-export default Header;
+    this.state = {
+      expanded: false
+    };
+  }
+
+  handleClick = () => {
+    this.setState(prevState => ({
+      expanded: !prevState.expanded
+    }));
+  };
+
+  handleSelection = (value, event) => {
+    console.log(value, event);
+  };
+
+  render() {
+    const menuItems = menuItemWords.map((word, i) => {
+      return (
+        <li key={i}>
+          <MenuItem className="navMenuItem">{word}</MenuItem>
+        </li>
+      );
+    });
+    return (
+      <header>
+        {/* <Nav /> */}
+        <div className="crossSection">
+          <div className="verticalTop" />
+          <div className="verticalBottom" />
+          <div className="horizontalLeft" />
+          <div className="horizontalRight" />
+        </div>
+        <Wrapper className="logo" onSelection={this.handleSelection}>
+          <Button className="logoButton">A D</Button>
+          <Menu className="navMenu">
+            <ul>{menuItems}</ul>
+          </Menu>
+        </Wrapper>
+      </header>
+    );
+  }
+}
