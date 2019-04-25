1c84ee1994ed31ab78e5eb942b7e5ba0f36c97bd
Anthony Agnone <anthonyagnone@Anthonys-MacBook-Pro.local>
2019-04-25 09:24:12 -0400

    Added header placeholder

--- src/components/App.js
+++ src/components/App.js
@@ -4,10 +4,12 @@ import { BrowserRouter as Router, Route } from 'react-router-dom'
 import Home from './newComponents/Home'
 import Post from './newComponents/Post'
 import Navigation from './newComponents/Navigation'
+import Header from './newComponents/Header'
 
 const App = () => (
   <Router>
     <div className='app-wrap'>
+      <Header />
       <main>
         <Route exact path='/' component={Home} />
         <Route path='/post/:slug' component={Post} />
---
+++ src/components/newComponents/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <header className='full-screen'>
+      <h1>I am the header</h1>
+    </header>
+  )
+}
+
+export default Header
